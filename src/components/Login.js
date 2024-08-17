3907d9e9d2013a32e092ee33b6663372887529be
Smeet Agrawal <smeet@skillr.ai>
2024-08-17 12:55:18 +0530

    Implemeneted Login form

---
+++ src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/20bf1f4d-1c73-48fd-8689-310d6dd80efc/81bdc063-cb8f-4afe-8a02-a3131ca4ef5e/IN-en-20240812-POP_SIGNUP_TWO_WEEKS-perspective_WEB_7998f3b6-63e3-424a-8328-550cf777ddce_small.jpg"
+          alt="background-logo"
+        />
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-2 w-full bg-gray-700 rounded-lg"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-2 w-full bg-gray-700 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-2 w-full bg-gray-700 rounded-lg"
+        />
+        <button type="submit" className="p-4 my-4 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+
+        <p className="my-4" onClick={toggleSignInForm}>
+          {isSignInForm ? "New to Netflix?" : "Already an user!"}{" "}
+          <span className="cursor-pointer hover:underline">
+            {isSignInForm ? "Sign Up" : "Sign In"}
+          </span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
